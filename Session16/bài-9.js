920a33f9de184ebc5278c46963352efdf04db0a5
Đăng Việt <trandangviet00@gmail.com>
2025-02-27 07:58:25 +0700

    Add files via upload

---
+++ Session16/bài-9.js
@@ -0,0 +1,4 @@
+let celsius = parseFloat(prompt("Nhập nhiệt độ (°C):"));
+let fahrenheit = (celsius * 9/5) + 32;
+console.log(`${celsius}°C = ${fahrenheit}°F`);
+alert(`${celsius}°C = ${fahrenheit}°F`);
